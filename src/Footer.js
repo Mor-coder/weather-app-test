3496afaea39aeb198b60cfefd7986a935b7b51e8
Mor-coder <morshmueli88@gmail.com>
2022-11-02 17:39:33 +0000

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,9 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>
-        <small>Open-source code, by Mor Shmueli</small>
+        <a href="https://github.com/Mor-coder/weather-app-test" target="_blank">
+          <small>Open-source code, by Mor Shmueli</small>
+        </a>
       </p>
     </div>
   );
